c110d5f0e72e6f1bea8aa2dbd84464ca31b8262d
shitou <shitou@bolo.em>
2022-03-02 15:21:36 +0800

    test mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hollo08/gotest3
+
+go 1.17
---
+++ say.go
@@ -0,0 +1,9 @@
+package go-test3
+
+import "fmt" 
+
+// say Hi to someone
+func SayHi(name string) string {
+   return fmt.Sprintf("Hi, %s", name)
+}
+
